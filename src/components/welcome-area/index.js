dec54873aa2e1063266cdf360183c53443a5f278
Praveen manchi <49606401+praveenmanchi@users.noreply.github.com>
2025-02-16 13:45:28 +0530

    live link added

--- src/components/welcome-area/index.js
+++ src/components/welcome-area/index.js
@@ -1,26 +1,30 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import './style.css'
-import LocationMap from '../LocationModal/LocationModal'
+import React from 'react';
+import './style.css';
+import LocationMap from '../LocationModal/LocationModal';
 
-const Welcome = () =>{
-    return(
+const Welcome = () => {
+    return (
         <div className="welcome-area">
             <div className="container">
                 <div className="row">
                     <div className="col-lg-12">
                         <div className="welcome-content">
-                            <h2>Please Join virtualy in our celebration</h2>
-                            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or less normal distribution of letters</p>
-                            <div className="btn"><Link to="https://www.youtube.com/live/rDwk4ubbKek?feature=share">Watch Live</Link></div>
+                            <h2>Please Join virtually in our celebration</h2>
+                            <p>
+                                It is a long-established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.
+                            </p>
+                            <div className="btn">
+                                <a href="https://www.youtube.com/live/rDwk4ubbKek" target="_blank" rel="noopener noreferrer">
+                                    Watch Live
+                                </a>
+                            </div>
                             {/* <LocationMap buttonClass={'location-btn'} /> */}
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
-    )
-}
+    );
+};
 
 export default Welcome;
